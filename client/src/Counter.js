7792b5f842a8addfdde47503d1651bb50a7bc3b7
Speuce <mrk5199@gmail.com>
2024-02-04 18:21:20 -0600

    Increment Counter Button

---
+++ client/src/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    // Send API request to update backend with the new count
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
